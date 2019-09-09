916405849c7ae181158c58ecacc016600820138c
MaxenceMillot <maxence.millot@hotmail.fr>
2019-09-09 16:45:19 +0200

    Modification Gradle : Room 1.0.0 vers 2.1.0

--- app/src/main/java/com/eni/lokacar/data/dal/dao/VehiculeDAO.java
+++ app/src/main/java/com/eni/lokacar/data/dal/dao/VehiculeDAO.java
@@ -12,59 +12,59 @@ public interface VehiculeDAO {
 
     // dispo
     @Query("SELECT * FROM Vehicule" +
-            " WHERE Vehicule.isDispo = :isDispo" +
-            " AND Vehicule.prixJour >= :prixMin" +
-            " AND Vehicule.prixJour <= :prixMax" +
-            " AND Vehicule.nbPlace >= :placesMin" +
-            " AND Vehicule.nbPlace <= :placesMax" +
-            " AND Vehicule.nbPorte >= :portesMin" +
-            " AND Vehicule.nbPorte <= :portesMax" +
-            " AND Vehicule.carburant = :carburant" +
-            " AND Vehicule.critair <= :critair")
+            " WHERE isDispo = :isDispo" +
+            " AND prixJour >= :prixMin" +
+            " AND prixJour <= :prixMax" +
+            " AND nbPlace >= :placesMin" +
+            " AND nbPlace <= :placesMax" +
+            " AND nbPorte >= :portesMin" +
+            " AND nbPorte <= :portesMax" +
+            " AND carburant = :carburant" +
+            " AND critair <= :critair")
     List<Vehicule> getAllDispo(Boolean isDispo, int prixMin, int prixMax, int placesMin, int placesMax, int portesMin, int portesMax, String carburant, int critair);
 
     // dispo + attelage
     @Query("SELECT * FROM Vehicule" +
-            " WHERE Vehicule.isDispo = :isDispo" +
-            " AND Vehicule.attelage = :attelage" +
-            " AND Vehicule.prixJour >= :prixMin" +
-            " AND Vehicule.prixJour <= :prixMax" +
-            " AND Vehicule.nbPlace >= :placesMin" +
-            " AND Vehicule.nbPlace <= :placesMax" +
-            " AND Vehicule.nbPorte >= :portesMin" +
-            " AND Vehicule.nbPorte <= :portesMax" +
-            " AND Vehicule.carburant = :carburant" +
-            " AND Vehicule.critair <= :critair")
+            " WHERE isDispo = :isDispo" +
+            " AND attelage = :attelage" +
+            " AND prixJour >= :prixMin" +
+            " AND prixJour <= :prixMax" +
+            " AND nbPlace >= :placesMin" +
+            " AND nbPlace <= :placesMax" +
+            " AND nbPorte >= :portesMin" +
+            " AND nbPorte <= :portesMax" +
+            " AND carburant = :carburant" +
+            " AND critair <= :critair")
     List<Vehicule> getAllDispoAttelage(Boolean isDispo, int prixMin, int prixMax, int placesMin, int placesMax, int portesMin, int portesMax, String carburant, int critair, Boolean attelage);
 
     // dispo + citadine
     @Query("SELECT * FROM Vehicule" +
-            " WHERE Vehicule.isDispo = :isDispo" +
-            " AND Vehicule.isCitadine = :isCitadine" +
-            " AND Vehicule.prixJour >= :prixMin" +
-            " AND Vehicule.prixJour <= :prixMax" +
-            " AND Vehicule.nbPlace >= :placesMin" +
-            " AND Vehicule.nbPlace <= :placesMax" +
-            " AND Vehicule.nbPorte >= :portesMin" +
-            " AND Vehicule.nbPorte <= :portesMax" +
-            " AND Vehicule.carburant = :carburant" +
-            " AND Vehicule.critair <= :critair")
-    List<Vehicule> getAllDispo(Boolean isDispo, int prixMin, int prixMax, int placesMin, int placesMax, int portesMin, int portesMax, String carburant, int critair, Boolean isCitadine);
+            " WHERE isDispo = :isDispo" +
+            " AND isCitadine = :isCitadine" +
+            " AND prixJour >= :prixMin" +
+            " AND prixJour <= :prixMax" +
+            " AND nbPlace >= :placesMin" +
+            " AND nbPlace <= :placesMax" +
+            " AND nbPorte >= :portesMin" +
+            " AND nbPorte <= :portesMax" +
+            " AND carburant = :carburant" +
+            " AND critair <= :critair")
+    List<Vehicule> getAllDispoCitadine(Boolean isDispo, int prixMin, int prixMax, int placesMin, int placesMax, int portesMin, int portesMax, String carburant, int critair, Boolean isCitadine);
 
     // dispo + attelage + citadine
     @Query("SELECT * FROM Vehicule" +
-            " WHERE Vehicule.isDispo = :isDispo" +
-            " AND Vehicule.attelage = :attelage" +
-            " AND Vehicule.isCitadine = :isCitadine" +
-            " AND Vehicule.prixJour >= :prixMin" +
-            " AND Vehicule.prixJour <= :prixMax" +
-            " AND Vehicule.nbPlace >= :placesMin" +
-            " AND Vehicule.nbPlace <= :placesMax" +
-            " AND Vehicule.nbPorte >= :portesMin" +
-            " AND Vehicule.nbPorte <= :portesMax" +
-            " AND Vehicule.carburant = :carburant" +
-            " AND Vehicule.critair <= :critair")
-    List<Vehicule> getAllDispo(Boolean isDispo, int prixMin, int prixMax, int placesMin, int placesMax, int portesMin, int portesMax, String carburant, int critair, Boolean attelage, Boolean isCitadine);
+            " WHERE isDispo = :isDispo" +
+            " AND attelage = :attelage" +
+            " AND isCitadine = :isCitadine" +
+            " AND prixJour >= :prixMin" +
+            " AND prixJour <= :prixMax" +
+            " AND nbPlace >= :placesMin" +
+            " AND nbPlace <= :placesMax" +
+            " AND nbPorte >= :portesMin" +
+            " AND nbPorte <= :portesMax" +
+            " AND carburant = :carburant" +
+            " AND critair <= :critair")
+    List<Vehicule> getAllDispoAttelageCitadine(Boolean isDispo, int prixMin, int prixMax, int placesMin, int placesMax, int portesMin, int portesMax, String carburant, int critair, Boolean attelage, Boolean isCitadine);
 
     @Insert
     void insert(Vehicule vehicule);
