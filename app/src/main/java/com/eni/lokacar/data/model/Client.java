715f29b9ce54f9278fab95d444a6e6d2a39adf69
Kylian Chaudemanche <kylian.chaudemanche@gmail.com>
2019-09-09 14:44:59 +0200

    constructeur Client

--- app/src/main/java/com/eni/lokacar/data/model/Client.java
+++ app/src/main/java/com/eni/lokacar/data/model/Client.java
@@ -5,4 +5,11 @@ public class Client {
     private String nom;
     private String prenom;
     private String telephone;
+
+    public Client(int id, String nom, String prenom, String telephone) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+    }
 }
