323cca3cd13ea93aa738cae83d214e8f41527385
Kylian Chaudemanche <kylian.chaudemanche@gmail.com>
2019-09-09 14:50:13 +0200

    ctor & Get/Set Client,Location et Vehicule

--- app/src/main/java/com/eni/lokacar/data/model/Location.java
+++ app/src/main/java/com/eni/lokacar/data/model/Location.java
@@ -11,4 +11,89 @@ public class Location {
     private int nbJours;
     private Float prix;
     private boolean isRendu;
+
+    public Location(int id, ArrayList<String> photoAvant, ArrayList<String> photoApres, Vehicule vehicule, Client client, int nbJours, Float prix, boolean isRendu) {
+        this.id = id;
+        this.photoAvant = photoAvant;
+        this.photoApres = photoApres;
+        this.vehicule = vehicule;
+        this.client = client;
+        this.nbJours = nbJours;
+        this.prix = prix;
+        this.isRendu = isRendu;
+    }
+
+    public Location(ArrayList<String> photoAvant, ArrayList<String> photoApres, Vehicule vehicule, Client client, int nbJours, Float prix, boolean isRendu) {
+        this.photoAvant = photoAvant;
+        this.photoApres = photoApres;
+        this.vehicule = vehicule;
+        this.client = client;
+        this.nbJours = nbJours;
+        this.prix = prix;
+        this.isRendu = isRendu;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<String> getPhotoAvant() {
+        return photoAvant;
+    }
+
+    public void setPhotoAvant(ArrayList<String> photoAvant) {
+        this.photoAvant = photoAvant;
+    }
+
+    public ArrayList<String> getPhotoApres() {
+        return photoApres;
+    }
+
+    public void setPhotoApres(ArrayList<String> photoApres) {
+        this.photoApres = photoApres;
+    }
+
+    public Vehicule getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(Vehicule vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public int getNbJours() {
+        return nbJours;
+    }
+
+    public void setNbJours(int nbJours) {
+        this.nbJours = nbJours;
+    }
+
+    public Float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Float prix) {
+        this.prix = prix;
+    }
+
+    public boolean isRendu() {
+        return isRendu;
+    }
+
+    public void setRendu(boolean rendu) {
+        isRendu = rendu;
+    }
 }
--- app/src/main/java/com/eni/lokacar/data/model/Vehicule.java
+++ app/src/main/java/com/eni/lokacar/data/model/Vehicule.java
@@ -13,4 +13,129 @@ public class Vehicule {
     private int critair;
     private boolean attelage;
     private boolean isCitadine;
+
+    public Vehicule(int id, String modele, String marque, String plaque, float prixJour, String photo, int nbPorte, int nbPlace, String carburant, int critair, boolean attelage, boolean isCitadine) {
+        this.id = id;
+        this.modele = modele;
+        this.marque = marque;
+        this.plaque = plaque;
+        this.prixJour = prixJour;
+        this.photo = photo;
+        this.nbPorte = nbPorte;
+        this.nbPlace = nbPlace;
+        this.carburant = carburant;
+        this.critair = critair;
+        this.attelage = attelage;
+        this.isCitadine = isCitadine;
+    }
+
+    public Vehicule(String modele, String marque, String plaque, float prixJour, String photo, int nbPorte, int nbPlace, String carburant, int critair, boolean attelage, boolean isCitadine) {
+        this.modele = modele;
+        this.marque = marque;
+        this.plaque = plaque;
+        this.prixJour = prixJour;
+        this.photo = photo;
+        this.nbPorte = nbPorte;
+        this.nbPlace = nbPlace;
+        this.carburant = carburant;
+        this.critair = critair;
+        this.attelage = attelage;
+        this.isCitadine = isCitadine;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getPlaque() {
+        return plaque;
+    }
+
+    public void setPlaque(String plaque) {
+        this.plaque = plaque;
+    }
+
+    public float getPrixJour() {
+        return prixJour;
+    }
+
+    public void setPrixJour(float prixJour) {
+        this.prixJour = prixJour;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public int getNbPorte() {
+        return nbPorte;
+    }
+
+    public void setNbPorte(int nbPorte) {
+        this.nbPorte = nbPorte;
+    }
+
+    public int getNbPlace() {
+        return nbPlace;
+    }
+
+    public void setNbPlace(int nbPlace) {
+        this.nbPlace = nbPlace;
+    }
+
+    public String getCarburant() {
+        return carburant;
+    }
+
+    public void setCarburant(String carburant) {
+        this.carburant = carburant;
+    }
+
+    public int getCritair() {
+        return critair;
+    }
+
+    public void setCritair(int critair) {
+        this.critair = critair;
+    }
+
+    public boolean isAttelage() {
+        return attelage;
+    }
+
+    public void setAttelage(boolean attelage) {
+        this.attelage = attelage;
+    }
+
+    public boolean isCitadine() {
+        return isCitadine;
+    }
+
+    public void setCitadine(boolean citadine) {
+        isCitadine = citadine;
+    }
 }
